4cb30ea93fec100a7796d21c9d0324d552d35802
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-09-16 15:11:40 +0100

    modify manifest with NoActionBar

---
+++ app/src/main/java/com/oauife/healthapp/HealthPage.kt
@@ -0,0 +1,11 @@
+package com.oauife.healthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicalPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medical_page)
+    }
+}
