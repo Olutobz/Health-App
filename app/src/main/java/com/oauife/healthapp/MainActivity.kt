34b88b9f4ac29282301d4985be0eb539eda888d9
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-09-16 15:17:14 +0100

    MainActivity

--- app/src/main/java/com/oauife/healthapp/MainActivity.kt
+++ app/src/main/java/com/oauife/healthapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.oauife.healthapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
+@Suppress("deprecation")
 class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 5000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed({
+            val intent = Intent(this, HealthPage::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME)
+
     }
 }
