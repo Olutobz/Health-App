dfa37bc18434442214d7e5ba722dc0cd1ebf9bd4
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-23 08:41:58 +0100

    add navigation components dependencies

--- app/src/main/java/com/oauife/healthapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/oauife/healthapp/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.oauife.healthapp
+package com.oauife.healthapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oauife.healthapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
