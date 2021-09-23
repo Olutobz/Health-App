3a0c3e62eca8839b63495f08d582671fa8c00e29
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-23 14:43:42 +0100

    add splash screen

--- app/src/main/java/com/oauife/healthapp/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/oauife/healthapp/ui/activities/SplashScreenActivity.kt
@@ -3,24 +3,11 @@ package com.oauife.healthapp.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager.LayoutParams.*
-import com.oauife.healthapp.R
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-
-        window.setFlags(
-            FLAG_FULLSCREEN,
-            FLAG_FULLSCREEN,
-        )
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
