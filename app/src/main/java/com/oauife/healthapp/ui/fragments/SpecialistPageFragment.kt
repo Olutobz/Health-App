b290f84563e9bb5c8b4f6a54ba9c6fdb6a66fe7a
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-29 04:02:11 +0100

    remove typo

--- app/src/main/java/com/oauife/healthapp/ui/fragments/SpecialistPageFragment.kt
+++ app/src/main/java/com/oauife/healthapp/ui/fragments/SpecialistPageFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.oauife.healthapp.R
 
-class SpecilistPageFragment : Fragment() {
+class SpecialistPageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
