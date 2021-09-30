b8a1530f7c8ab169b48182194355808a500a84b4
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-09-30 07:31:33 +0100

    complete specialist login page

--- app/src/main/java/com/oauife/healthapp/ui/fragments/SpecialistLoginFragment.kt
+++ app/src/main/java/com/oauife/healthapp/ui/fragments/SpecialistLoginFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.oauife.healthapp.R
 
-class SpecialistPageFragment : Fragment() {
+class SpecialistLoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
